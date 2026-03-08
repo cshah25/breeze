62bb136f12f3ab8136f0bdf3abe8413dcb880bd4
Osman Akman <oakmantr@gmail.com>
2026-03-07 21:28:35 -0700

    Fixed some inconsistencies

--- code/app/src/main/java/com/example/breeze_seas/User.java
+++ code/app/src/main/java/com/example/breeze_seas/User.java
@@ -23,6 +23,30 @@ public class User {
         this.phoneNumber = null;
         this.isAdmin = false;
         this.notificationEnabled = true;
+        this.createdAt = null;
+        this.updatedAt = null;
+    }
+
+    public User(String deviceId, String userName, String email) {
+        this.deviceId = deviceId;
+        this.userName = userName;
+        this.email = email;
+        this.phoneNumber = null;
+        this.isAdmin = false;
+        this.notificationEnabled = true;
+        this.createdAt = null;
+        this.updatedAt = null;
+    }
+
+    public User(String deviceId, String userName, String email, String phoneNumber) {
+        this.deviceId = deviceId;
+        this.userName = userName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.isAdmin = false;
+        this.notificationEnabled = true;
+        this.createdAt = null;
+        this.updatedAt = null;
     }
 
     public User(String userName, String email, String deviceId, String phoneNumber,
