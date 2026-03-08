6da2643ff833e1afe137750e367a4bb9efd946d1
MoonTzu76 <oakmantr@gmail.com>
2026-03-08 17:33:14 -0600

    Added edit buttons to profile fragment

--- code/app/src/main/java/com/example/breeze_seas/ProfileFragment.java
+++ code/app/src/main/java/com/example/breeze_seas/ProfileFragment.java
@@ -1,5 +1,10 @@
 package com.example.breeze_seas;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
+
 /*** ProfileFragment is a top-level destination accessed via Bottom Navigation.
  *
  * <p>Current state:* - A placeholder screen was created to validate the navigation wiring and theme style.
@@ -7,9 +12,13 @@ import androidx.fragment.app.Fragment;
  */
 public class ProfileFragment extends Fragment {
 
+    private TextInputEditText editName;
+
+
     public ProfileFragment() {
         super(R.layout.fragment_profile);
     }
+
 }
 
 
