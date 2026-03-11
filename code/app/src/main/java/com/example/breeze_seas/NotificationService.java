2da4b93484a41bb5ab55118d4a88c6286c239cca
MoonTzu76 <oakmantr@gmail.com>
2026-03-10 22:59:22 -0600

    created notification class

---
+++ code/app/src/main/java/com/example/breeze_seas/NotificationService.java
@@ -0,0 +1,12 @@
+package com.example.breeze_seas;
+
+import android.graphics.drawable.Drawable;
+
+public class NotificationService {
+
+    private String notificationBody;
+    private Drawable notificationIcon;
+
+
+
+}
